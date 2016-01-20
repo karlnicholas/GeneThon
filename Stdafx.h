73f407bbbdb0fc517516f064522ce6d5d3379a7f
Karl Nicholas <Karl.Nicholas@outlook.com>
2016-01-20 01:27:50 -0500

    More cleanup

--- Stdafx.h
+++ Stdafx.h
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#define _WIN32_WINNT 0x0502
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <afxwin.h>         // MFC core and standard components
